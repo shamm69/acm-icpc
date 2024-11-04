cd21d06e083bd990844ff6e5840b7f229cc2b66a
shamm69 <shamowo69@gmail.com>
2024-11-05 01:43:06 +0530

    Create solvemefirst.cpp

---
+++ solvemefirst.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solveMeFirst (int a ,int b)
+{
+    int sum = a+b;
+    return sum;
+}
+
+int main ()
+{
+    int a,b;
+    cin >> a >> b;
+    int r = solveMeFirst(a,b);
+    cout << r;
+    return 0;
+}
